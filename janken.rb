58b4baeff01859e19e4d4256efaef888ec6b559f
EC2 Default User <ec2-user@ip-172-31-10-101.us-east-2.compute.internal>
2021-03-09 23:34:24 +0000

    secound commit

--- janken.rb
+++ janken.rb
@@ -1,29 +1,54 @@
 loop do
   puts '-----------------------------------------------'
   puts "じゃんけんの女神、ハマーベー・ミナーミーが現れた。"
-  puts "私とデートしたいならじゃんけんとあっち向いてホイで勝ったみなさい"
-　puts "勝負する:1逃げる:2"
-　
-　choice = gets.chomp.to_i
+  puts "私とデートしたいならじゃんけんとあっち向いてホイで勝ってみなさい"
+  puts "勝負する:1逃げる:2"
+  
+  choice = gets.chomp
 
   case choice
     when '1'
       puts '-----------------------------------------------'
-      puts 'グー:1 チョキ:2 パー:3'
+      puts 'グー:0 チョキ:1 パー:2'
       puts "じゃんけん...."
       
       my_hand=gets.chomp.to_i
-      enemy_hand=rand(1..3)
+      enemy_hand=rand(3)
+      jankens=["グー","チョキ","パー"]
 
       if my_hand==enemy_hand
-        puts "あなた:#{my_hand} ミナーミー:#{enemy_hand}"
+        puts "あなた:#{jankens[my_hand]} ミナーミー:#{jankens[enemy_hand]}"
         puts "引き分けざます。"
+        puts "あいこで。。。"
+        puts "もう一度初めからよ"
       elsif (my_hand==1 && enemy_hand==2) || (my_hand==2 && enemy_hand==1) || (my_hand==3 && enemy_hand==1) 
-        puts "あなた:#{my_hand} ミナーミー:#{enemy_hand}"
-        puts "あなたの勝ちよ。仕方ないわね、デートは一回だけよ"
+        puts "あなた:#{jankens[my_hand]} ミナーミー:#{jankens[enemy_hand]}"
+        puts "あなたの勝ちよ。あっちむいてホイで勝ってみなさい"
+        puts "------------------------------------------------"
+        puts "上:0 下:1 右:2 左:3"
+        puts "あっちむいて。。。。。"
+        
+        my_finger=gets.chomp.to_i
+        enemy_finger=rand(4)
+        attimuite=["上","下","右","左"]
+        puts "ホイ"
+        puts "--------------------------------"
+        puts "あなた:#{attimuite[my_finger]} ミナーミー:#{attimuite[enemy_finger]}を出しました。"
+        
+        if my_finger==enemy_finger
+          puts "あなたの勝ちよ、一回だけデートしてあげる。"
+        else
+          puts "惜しかったわね。"
+          break
+          
+        end
+          
+          
       else
-        puts "あなた:#{my_hand} ミナーミー:#{enemy_hand}"
+        puts "あなた:#{jankens[my_hand]},ミナーミー:#{jankens[enemy_hand]}"
         puts "あなたの負けよ。わたしとデートなんて一万年はやいわ。"
+        break
+        
       end
     when '2'
       puts"------------------------------------------------"
